815f234b1d502280d6a52445f999fca8287f5197
Johann <jayc.4774@gmail.com>
2019-01-31 20:28:37 -0500

    version 3

--- app/src/main/java/icesi/com/myapplication/MainActivity.java
+++ app/src/main/java/icesi/com/myapplication/MainActivity.java
@@ -1,13 +1,53 @@
 package icesi.com.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_register;
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("SALIR")
+                .setMessage("¿Desea Salir?")
+                .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                }).setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                   MainActivity.super.onBackPressed();
+                    }
+                });
+        builder.show();
+
+
+        super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tv_register = findViewById(R.id.tv_register);
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Register.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/icesi/com/myapplication/Register.java
+++ app/src/main/java/icesi/com/myapplication/Register.java
@@ -3,11 +3,11 @@ package icesi.com.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class second extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_register);
     }
 }
