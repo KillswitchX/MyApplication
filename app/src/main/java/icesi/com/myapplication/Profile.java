d2cb19db04b10944fafd06b999b8d284deacdfc0
Johann <jayc.4774@gmail.com>
2019-01-31 20:57:59 -0500

    version 6

--- app/src/main/java/icesi/com/myapplication/Profile.java
+++ app/src/main/java/icesi/com/myapplication/Profile.java
@@ -1,7 +1,9 @@
 package icesi.com.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        //Intent con el que fue llamado
+        Intent datos = getIntent();
+
+        String nom = datos.getExtras().getString("nombre");
+
+        String pass = datos.getExtras().getString("contra");
+
+        Toast.makeText(this, nom + " , " + pass, Toast.LENGTH_LONG).show();
     }
 }
