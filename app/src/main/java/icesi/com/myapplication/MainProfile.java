cce23e00c119631b1491b66181daf5fe06712b0f
Johann <jayc.4774@gmail.com>
2019-02-07 20:33:44 -0500

    version 7

--- app/src/main/java/icesi/com/myapplication/MainProfile.java
+++ app/src/main/java/icesi/com/myapplication/MainProfile.java
@@ -1,13 +1,43 @@
 package icesi.com.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainProfile extends AppCompatActivity {
+public class MainProfile extends FragmentActivity {
+
+    private Fragment_A fragmentA;
+
+    private Fragment_B fragmentB;
+
+    private Fragment_C fragmentC;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_profile);
+
+        fragmentA = new Fragment_A();
+        fragmentB = new Fragment_B();
+        fragmentC = new Fragment_C();
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+
+                return true;
+            }
+        });
+
+
+
     }
 }
