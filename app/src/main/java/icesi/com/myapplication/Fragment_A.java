7722b969d9dc095e27ce0e4481eba2c2c1b14cba
Johann <jayc.4774@gmail.com>
2019-02-07 20:50:53 -0500

    version 7

--- app/src/main/java/icesi/com/myapplication/Fragment_A.java
+++ app/src/main/java/icesi/com/myapplication/Fragment_A.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Fragment_A extends Fragment {
@@ -24,6 +25,10 @@ public class Fragment_A extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+
+     // Inflate permite pasar de xml a objeto
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        Button btn_a_fragment_a = v.findViewById(R.id.btn_a_fragment_a);
+        return v;
     }
 }
--- app/src/main/java/icesi/com/myapplication/MainProfile.java
+++ app/src/main/java/icesi/com/myapplication/MainProfile.java
@@ -3,6 +3,8 @@ package icesi.com.myapplication;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -14,7 +16,6 @@ public class MainProfile extends FragmentActivity {
     private Fragment_B fragmentB;
 
     private Fragment_C fragmentC;
-
 
 
     @Override
@@ -31,12 +32,28 @@ public class MainProfile extends FragmentActivity {
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
 
+                switch (menuItem.getItemId()) {
+                    case R.id.menubar_home:
+                        transaction.replace(R.id.contenido, fragmentA);
+                        transaction.commit();
+                        break;
+                    case R.id.menubar_notification:
+                        transaction.replace(R.id.contenido, fragmentB);
+                        transaction.commit();
+                        break;
+
+                    case R.id.menubar_feed:
+                        transaction.replace(R.id.contenido, fragmentC);
+                        transaction.commit();
+                        break;
+                }
 
                 return true;
             }
         });
-
 
 
     }
